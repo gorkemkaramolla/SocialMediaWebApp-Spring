664abe3db8702c4b0a7a589c95fe9208b05514c0
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-08-24 10:49:02 +0300

    Id added

--- src/main/java/com/example/javalearnbook/dto/requests/CommentsRequests.java
+++ src/main/java/com/example/javalearnbook/dto/requests/CommentsRequests.java
@@ -5,10 +5,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 @Getter
 @NoArgsConstructor
 @Setter
 public class CommentsRequests {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
     private String comment;
     private Long postId;
