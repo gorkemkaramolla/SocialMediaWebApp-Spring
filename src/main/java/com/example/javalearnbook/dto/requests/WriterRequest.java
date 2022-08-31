da7691c296a5532aff3eb45c8fa747a3bec6bec4
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-08-31 21:54:44 +0300

    REQUEST DTO FOR WRITER(USER)

---
+++ src/main/java/com/example/javalearnbook/dto/requests/WriterRequest.java
@@ -0,0 +1,11 @@
+package com.example.javalearnbook.dto.requests;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class WriterRequest {
+    private String email;
+    private String password;
+}
