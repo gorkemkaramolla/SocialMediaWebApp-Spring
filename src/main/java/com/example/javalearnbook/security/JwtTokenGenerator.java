73c86b6f6c053c5336ed0192a7811671e5b27fcd
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-08-31 21:53:11 +0300

    JWT TOKEN GENERATOR

--- src/main/java/com/example/javalearnbook/security/JwtTokenGenerator.java
+++ src/main/java/com/example/javalearnbook/security/JwtTokenGenerator.java
@@ -1,13 +1,55 @@
 package com.example.javalearnbook.security;
 
+import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JwtTokenGenerator {
     @Value("${chatapp.application.secret}")
     private String APP_SECRET;
-    @Value("${chatapp.application.expiration}")
+    @Value("${chatapp.expiration.in}")
     private Long EXPIRES_IN;
+
+    public String generateJWT(Authentication auth)
+    {
+        JwtUserDetails userDetails = (JwtUserDetails) auth.getPrincipal();
+        Date now = new Date(System.currentTimeMillis());
+        Date expirationDate = new Date(now.getTime() + EXPIRES_IN);
+        return Jwts.builder()
+                .setSubject(userDetails.getId().toString())
+                .setIssuedAt(now).setExpiration(expirationDate)
+                .signWith(SignatureAlgorithm.HS512,APP_SECRET).compact();
+    }
+    public Long getWriterIdFromJWT(String token)
+    {
+
+        return Long.parseLong(parseGetBody(token).getSubject());
+    }
+    public boolean validateToken(String token)
+    {
+        try {
+            Jwts.parser().setSigningKey(APP_SECRET).parseClaimsJws(token);
+            return !isTokenExpired(token);
+        }
+        catch (SignatureException | UnsupportedJwtException | IllegalArgumentException | ExpiredJwtException |
+               MalformedJwtException exception) {
+            return false;
+        }
+    }
+    private boolean isTokenExpired(String token) {
+        Claims claims = parseGetBody(token);
+        Date dateCreated = claims.getIssuedAt();
+        return !dateCreated.before(claims.getExpiration());
+
+    }
+    private Claims parseGetBody(String token)
+    {
+        return  Jwts.parser().setSigningKey(APP_SECRET)
+                .parseClaimsJws(token).getBody();
+    }
 
 }
