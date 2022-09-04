8b31e7b1ea127d46de711ac484971c49c0f4e55e
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-04 22:40:49 +0300

    Response Object for Authorization

---
+++ src/main/java/com/example/javalearnbook/dto/responses/security/AuthResponse.java
@@ -0,0 +1,11 @@
+package com.example.javalearnbook.dto.responses.security;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class AuthResponse {
+    private Long id;
+    private String message;
+}
