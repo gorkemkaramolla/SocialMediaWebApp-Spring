72437c8b29e32d7bef071bc421ae8b05333fda3a
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-07 22:34:33 +0300

    cookie added

--- src/main/java/com/example/javalearnbook/dto/responses/security/AuthResponse.java
+++ src/main/java/com/example/javalearnbook/dto/responses/security/AuthResponse.java
@@ -2,10 +2,14 @@ package com.example.javalearnbook.dto.responses.security;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.http.ResponseCookie;
+
+import javax.servlet.http.Cookie;
 
 @Getter
 @Setter
 public class AuthResponse {
     private Long id;
     private String message;
+    private Cookie httpOnly;
 }
