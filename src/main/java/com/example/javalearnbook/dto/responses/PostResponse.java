744c3f1797c1378bc5cec461bf54de358fc2edc3
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-09 23:43:53 +0300

    unused import

--- src/main/java/com/example/javalearnbook/dto/responses/PostResponse.java
+++ src/main/java/com/example/javalearnbook/dto/responses/PostResponse.java
@@ -4,7 +4,6 @@ import com.example.javalearnbook.model.Post;
 import com.example.javalearnbook.model.PostLike;
 import lombok.*;
 
-import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 
