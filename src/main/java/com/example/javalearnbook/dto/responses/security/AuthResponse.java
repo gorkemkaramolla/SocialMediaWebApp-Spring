6c71db73e29022d993924f2ac5c20ab77df2ca09
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-09 23:42:37 +0300

    data type changed

--- src/main/java/com/example/javalearnbook/dto/responses/security/AuthResponse.java
+++ src/main/java/com/example/javalearnbook/dto/responses/security/AuthResponse.java
@@ -1,11 +1,8 @@
 package com.example.javalearnbook.dto.responses.security;
 
-import com.example.javalearnbook.model.RefreshToken;
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.http.ResponseCookie;
 
-import javax.servlet.http.Cookie;
 
 @Getter
 @Setter
