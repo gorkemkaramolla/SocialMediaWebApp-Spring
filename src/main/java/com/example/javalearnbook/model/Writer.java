1b66c69aac5b232d7606bfccbf62a94512a0bfe4
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-11 13:56:16 +0300

    field changes

--- src/main/java/com/example/javalearnbook/model/Writer.java
+++ src/main/java/com/example/javalearnbook/model/Writer.java
@@ -25,8 +25,7 @@ public class Writer {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String name;
-    private String lastName;
+    private String userName;
     private String bio;
     @Column(unique = true)
     private String email;
