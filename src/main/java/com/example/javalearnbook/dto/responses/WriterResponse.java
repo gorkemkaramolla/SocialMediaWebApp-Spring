95e87ed6004ad75fefd6f8042af7582dc2928544
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-11 13:56:53 +0300

    Writer Response Dto created

---
+++ src/main/java/com/example/javalearnbook/dto/responses/WriterResponse.java
@@ -0,0 +1,10 @@
+package com.example.javalearnbook.dto.responses;
+
+import lombok.Data;
+
+@Data
+public class WriterResponse {
+    private String userName;
+    private String bio;
+    private String email;
+}
