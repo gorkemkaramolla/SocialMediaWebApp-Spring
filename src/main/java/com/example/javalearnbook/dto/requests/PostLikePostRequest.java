6bedb8dc38842728be8db2491b818385f2a1d607
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-27 14:54:30 +0300

    Writer id Converted to string

--- src/main/java/com/example/javalearnbook/dto/requests/PostLikePostRequest.java
+++ src/main/java/com/example/javalearnbook/dto/requests/PostLikePostRequest.java
@@ -14,7 +14,7 @@ public class  PostLikePostRequest  {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private Long writerId;
+    private String writerId;
     private Long postId;
 
 }
