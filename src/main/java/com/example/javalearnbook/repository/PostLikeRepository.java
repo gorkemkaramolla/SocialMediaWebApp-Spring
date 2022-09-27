9d934913a923321065738532a91a6efeb8564c39
gorkemkaramolla <gorkem_karamolla@hotmail.com>
2022-09-27 14:54:44 +0300

    Find by Writer Id and Post Id

--- src/main/java/com/example/javalearnbook/repository/PostLikeRepository.java
+++ src/main/java/com/example/javalearnbook/repository/PostLikeRepository.java
@@ -10,4 +10,6 @@ import java.util.List;
 @Repository
 public interface PostLikeRepository extends JpaRepository<PostLike,Long> {
     List<PostLike> findByPostId(Long postId);
+
+    PostLike findByWriterIdAndPostId(Long writerId,Long PostId);
 }
